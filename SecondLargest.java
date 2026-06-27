7d0a9990c0d5b4a6c348d20b489054bea24ec3cf
AROMAL NAIR <YOUR_GITHUB_EMAIL>
2026-06-27 13:20:06 +0530

    second largest number.java

---
+++ SecondLargest.java
@@ -0,0 +1,31 @@
+public class SecondLargest{
+    public static int findSecondLargest(int []arr){
+        if(arr.length<2){
+            return -1;
+        }
+        int largest=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>largest){
+                second  = largest;
+                largest=num;
+            }
+            else if(num>second && num!=largest){
+                            second =num;
+
+
+            }
+
+        }
+        if(second==Integer.MIN_VALUE){
+            return -1;
+
+        }
+        return second ;
+
+    }
+    public static void main(String []args){
+        int[] arr={2,3,4,5,16,7,8};
+        System.out.println(findSecondLargest(arr));
+    }
+}
