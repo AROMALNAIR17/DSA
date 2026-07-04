0912c6a4708e0293c042938e1119b65110bf924d
AROMAL NAIR <aromalaromal024@gmail.com>
2026-07-04 19:33:06 +0530

    Problem 13: Binary Search (LeetCode 704)

---
+++ binarysearch.java
@@ -0,0 +1,28 @@
+public class binarysearch{
+    public static int search(int[]nums,int target ) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+
+            }
+            else{
+                right=mid-1;
+            }
+
+        }
+            return -1;
+
+        
+    }
+    public static void main(String[] args) {
+        int[] nums ={2,4,5,6,7,8,9};
+        System.out.println(search(nums,9));
+    }
+}
