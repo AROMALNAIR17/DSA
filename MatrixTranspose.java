1174fe08d7206b04c92e20dff04e1ea51b9ec50a
AROMAL NAIR <aromalaromal024@gmail.com>
2026-07-05 01:07:31 +0530

    Matrix Transpose (LeetCode 867)

---
+++ MatrixTranspose.java
@@ -0,0 +1,27 @@
+public class MatrixTranspose{
+    public static int[][] transpose(int[][] matrix){
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int[][]ans=new int[col][row];
+        for (int  i =0; i < row; i++) {
+            for(int j=0; j<col;j++){
+                ans[j][i] = matrix[i][j];
+
+            }
+            
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3},{4,5,6}};
+        int [][] ans =transpose(matrix);
+        for (int i = 0; i <ans.length; i++) {
+            for(int j=0;j<ans[0].length;j++){
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+            
+        
+    }
+}
