e4fe9dfa8883e7b2f6da7485a3838302c5fd622d
AROMAL NAIR <aromalaromal024@gmail.com>
2026-07-09 19:59:51 +0530

    Sort Colors (LeetCode 75)

---
+++ SortColors.java
@@ -0,0 +1,39 @@
+public class SortColors{
+    public static void sort(int[] nums){
+        int low=0;
+        int mid=0;
+        int  high =nums.length-1;
+        
+        while (mid<=high) {
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            } 
+            else if (nums[mid]==1) {
+                mid++;
+                
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+
+
+            }
+            
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums={1,1,2,2,0,0};
+        sort(nums);
+        for(int num:nums){
+            System.out.print(num);
+        }
+
+
+    }
+}
